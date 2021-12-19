980051944d031626457b9099114ffb4db598114d
fanxiangling <xbackfan@didichuxing.com>
2021-12-19 14:46:56 +0800

    feat: add form-create-designer from github and add .eslintrc.js

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
-console.log('-main.js-', router)
+
+import FcDesigner from '../fcDesigner/index'
+Vue.component('FcDesigner', FcDesigner)
 
 Vue.config.productionTip = false
+
 
 new Vue({
   router,
